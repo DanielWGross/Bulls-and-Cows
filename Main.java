e6065310226abba9e3535d7b60a9d3be14239d67
Dan Gross <danwgross@gmail.com>
2022-09-21 16:23:25 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("The secret code is prepared: ****.\n\n");
+        System.out.format("Turn 1. Answer:\n");
+        System.out.format("1234\n");
+        System.out.format("Grade: None.\n\n");
+        System.out.format("Turn 2. Answer:\n");
+        System.out.format("9876\n");
+        System.out.format("Grade: 4 bulls.\n");
+        System.out.format("Congrats! The secret code is 9876.\n");
+    }
+}
